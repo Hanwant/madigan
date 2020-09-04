0153bc07e13ccf1572a9b5955ef9c51d56843de6
Hanwant <admin@madigan.tech>
2020-09-04 17:28:14 +1200

    updated tests

--- madigan/tests/dqn_test.py
+++ madigan/tests/dqn_test.py
@@ -51,35 +51,6 @@ def test_conv_forward_pass():
     assert tuple(action.shape) == (1, config.n_assets)
 
 
-def train_step(self, sarsd, device):
-    import torch
-    import torch.nn.functional as F
-    self.opt.zero_grad()
-
-    with torch.no_grad():
-        if self.double_dqn:
-            pass
-        else:
-            next_states = State(torch.tensor(sarsd.next_state.price, dtype=torch.float32).to(device),
-                                torch.tensor(sarsd.next_state.port, dtype=torch.float32).to(device))
-            qvals_next = self.model_t(next_states).max(-1)[0]
-        rewards = torch.tensor(sarsd.reward, dtype=torch.float32, device=device).unsqueeze(-1)
-        done_mask = ~torch.tensor(sarsd.done, dtype=torch.bool, device=device).unsqueeze(-1)
-        G_t = rewards + (self.discount * done_mask * qvals_next)
-
-    actions = torch.tensor(sarsd.action, dtype=torch.long, device=device).unsqueeze(-1)
-    actions_mask = F.one_hot(actions, self.action_atoms).to(device)
-    states = State(torch.tensor(sarsd.state.price, dtype=torch.float32).to(device),
-                        torch.tensor(sarsd.state.port, dtype=torch.float32).to(device))
-    qvals = self.model_b(states)
-    Q_t = (qvals * actions_mask).sum(-1)
-    td_error = (G_t - Q_t)
-    loss = (td_error**2).mean()
-    loss.backward()
-    self.opt.step()
-    return {'loss': loss.detach().item(), 'td_error': td_error, 'G_t': G_t, 'Q_t': Q_t}
-
-
 @pytest.mark.skip()
 def test_conv_batch_overfit():
     config = make_config(discrete_actions=True, discrete_action_atoms=11, min_tf=64,
@@ -101,8 +72,6 @@ def test_conv_batch_overfit():
             print('step: ', i, 'loss: ', f"{metrics['loss']:.2e}", 'td_error: ', '{:.2e}'.format(metrics['td_error']))
     qvals_match = [torch.allclose(q[0], q[1]) for q in qvals]
     assert sum(qvals_match) > 1
-
-
 
 
 if __name__=="__main__":
--- madigan/tests/replay_buffer_test.py
+++ madigan/tests/replay_buffer_test.py
@@ -2,10 +2,12 @@ import timeit
 from collections import deque
 from random import sample
 import numpy as np
-from madigan.utils import SARSD, time_profile
-from madigan.utils import replay_buffer
+from madigan.utils import SARSD, State, time_profile
+from madigan.utils import ReplayBuffer
+import torch
 
-def test_basic_buffers():
+
+def test_data_structures():
     # _list = list(range(1000000))
     # _deque= deque(_list)
     # _np = np.arange(1000000)
@@ -32,16 +34,27 @@ def test_basic_buffers():
         for i in range(1000000):
             _np[i] = sarsd
         return _np
+    # def blist_grow():
+    #     _list = blist()
+    #     for i in range(1000000):
+    #         _list.append(sarsd)
+    #     return _list
+    # def blist_allocate():
+    #     _list = blist([None])*1000000
+    #     for i in range(1000000):
+    #         _list[i] = sarsd
+    #     return _list
 
     print('\n')
     print("="*80)
-    print("Testing replay buffer allocation and sampling methods \n")
+    print("Testing data structures for allocation and sampling methods \n")
     print("="*80)
     print("1. Data Structures for allocation \n")
 
     out = time_profile(10, 1, list_grow____=list_grow, list_allocate=list_allocate,
-                       deque_grow___=deque_grow,np_allocate__=np_allocate)
-    l, d, a = out['list_grow____'], out['deque_grow___'], out['np_allocate__']
+                       deque_grow___=deque_grow, np_allocate__=np_allocate,)
+                       # blist_grow___=blist_grow, blist_allocate=blist_allocate)
+    l, d, a = out['list_grow____'], out['deque_grow___'], out['np_allocate__']#, out['blist_grow___']
 
     def list_sample():
         _sample = sample(l, 32)
@@ -49,13 +62,110 @@ def test_basic_buffers():
         _sample = sample(d, 32)
     def np_sample():
         _sample = np.random.choice(a, 32)
+    # def blist_sample():
+    #     _sample = sample(b, 32)
 
     print("="*80)
     print("2. Data Structures for sampling \n")
     time_profile(1000, 0, list_sample_=list_sample,
                  deque_sample=deque_sample,
-                 np_sample___=np_sample)
+                 np_sample___=np_sample,)
+                 # blist_sample=blist_sample)
+    print("="*80)
+
+def test_replay_buffer():
+    size = 100_000
+    rb = ReplayBuffer(size)
+    state = State(np.random.randn(12, 64), np.random.randn(12))
+    sarsd = SARSD(state, 1, 2., state, False)
+
+    def sample_2(self, n):
+        idx = sample(range(self.filled), n)
+        state_price = np.array([self._buffer[i].state.price for i in idx])
+        state_port = np.array([self._buffer[i].state.port for i in idx])
+        state = State(state_price, state_port)
+        reward = np.array([self._buffer[i].reward for i in idx])
+        action = np.array([self._buffer[i].action for i in idx])
+        next_state_price = np.array([self._buffer[i].next_state.price for i in idx])
+        next_state_port = np.array([self._buffer[i].next_state.port for i in idx])
+        next_state = State(next_state_price, next_state_port)
+        done = np.array([self._buffer[i].done for i in idx])
+        return SARSD(state, action, reward, next_state, done)
+
+    def sample_3(self, n):
+        idx = sample(range(self.filled), n)
+        state_price = torch.tensor([self._buffer[i].state.price for i in idx])
+        state_port = torch.tensor([self._buffer[i].state.port for i in idx])
+        state = State(state_price, state_port)
+        reward = torch.tensor([self._buffer[i].reward for i in idx])
+        action = torch.tensor([self._buffer[i].action for i in idx])
+        next_state_price = torch.tensor([self._buffer[i].next_state.price for i in idx])
+        next_state_port = torch.tensor([self._buffer[i].next_state.port for i in idx])
+        next_state = State(next_state_price, next_state_port)
+        done = torch.tensor([self._buffer[i].done for i in idx])
+        return SARSD(state, action, reward, next_state, done)
+
+    def sample_4(self, n):
+        sarsd = self.sample(n)
+        state = State(torch.tensor(sarsd.state.price), torch.tensor(sarsd.state.port))
+        action = torch.tensor(sarsd.action)
+        next_state = State(torch.tensor(sarsd.next_state.price), torch.tensor(sarsd.next_state.port))
+        reward = torch.tensor(sarsd.reward)
+        done = torch.tensor(sarsd.done)
+        return SARSD(state, action, reward, next_state, done)
+
+    def sample_5(self, n):
+        sarsd = sample_2(rb, n)
+        state = State(torch.tensor(sarsd.state.price), torch.tensor(sarsd.state.port))
+        action = torch.tensor(sarsd.action)
+        next_state = State(torch.tensor(sarsd.next_state.price), torch.tensor(sarsd.next_state.port))
+        reward = torch.tensor(sarsd.reward)
+        done = torch.tensor(sarsd.done)
+        return SARSD(state, action, reward, next_state, done)
+
+    def sample_6(self, n):
+        _sample = sample(self._buffer, n)
+        state_price = np.stack([s.state.price for s in _sample])
+        state_port = np.stack([s.state.port for s in _sample])
+        state = State(state_price, state_port)
+        next_state_price = np.stack([s.next_state.price for s in _sample])
+        next_state_port = np.stack([s.next_state.port for s in _sample])
+        next_state = State(next_state_price, next_state_port)
+        action = np.array([s.action for s in _sample])
+        reward = np.array([s.reward for s in _sample])
+        done = np.array([s.done for s in _sample])
+        return SARSD(state, action, reward, next_state, done)
+
+    def populate_buffer():
+        for i in range(size):
+            rb.add(sarsd)
+    def sample_buffer():
+        rb.sample(32)
+    def sample_buffer_2():
+        sample_2(rb, 32)
+    def sample_buffer_3():
+        sample_3(rb, 32)
+    def sample_buffer_4():
+        sample_4(rb, 32)
+    def sample_buffer_5():
+        sample_5(rb, 32)
+    def sample_buffer_6():
+        sample_6(rb, 32)
+
+    print('\n')
+    print("="*80)
+    print("Testing implemented replay buffer allocation and sampling methods \n")
+    print("="*80)
+    print("1. Allocation \n")
+    time_profile(10, 0, populate_buffer=populate_buffer)
+    print("="*80)
+    print("2. Sampling \n")
+    time_profile(1000, 0, sample_buffer_default____=sample_buffer, sample_buffer_def_v2_____=sample_buffer_2,
+                 sample_buffer_torch______=sample_buffer_3, sample_buffer_np_to_torch=sample_buffer_4,
+                 sample_buffer_np_torch_v2=sample_buffer_5, sample_buffer_np_stack___=sample_buffer_6)
+    print("="*80)
 
 
 if __name__=="__main__":
-    test_basic_buffers()
+    test_data_structures()
+    test_replay_buffer()
