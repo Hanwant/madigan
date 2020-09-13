33b0fb3020f1bcbe69eaa820239cf741a084030b
Hanwant <admin@madigan.tech>
2020-09-13 14:16:09 +1200

    added untracked logging

---
+++ madigan/utils/logging.py
@@ -0,0 +1,20 @@
+from typing import Union
+from pathlib import Path
+import pandas as pd
+
+def save_to_hdf(path: Union[str, Path], key: str, df: pd.DataFrame, append_if_exists: bool=True):
+    """
+    Generic for saving df to hdf5 (and appendind by default - useful for
+    fast i/o of logs)
+    """
+    with pd.HDFStore(path, mode='a') as f:
+        f.put(key, df, format='t', append=append_if_exists)
+
+def load_from_hdf(path: Union[str, Path], key: str):
+    """
+    Generic for loading df from hdf5 (useful for fast i/o of logs)
+    """
+    with pd.HDFStore(path, mode='r') as f:
+        if '/'+key in f.keys():
+            return pd.read_hdf(f, key=key)
+    return None
