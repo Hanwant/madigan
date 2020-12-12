caebf12a89ce1324611a6ed85013b63e04000eb4
Hanwant <admin@madigan.tech>
2020-12-12 17:52:14 +1300

    Refactor __init__ and utils

--- madigan/modelling/__init__.py
+++ madigan/modelling/__init__.py
@@ -1,12 +1,1 @@
-from .algorithm.dqn import DQN, DQNReverser
-from .algorithm.iqn import IQN
-from .algorithm.ddpg import DDPG, DDPGDiscretized
-# from .net.mlp_model import MLPNet
-
-
-def make_agent(config):
-    if config.agent_type in ("DQN", "DQNReverser", "IQN", "DDPG",
-                             "IQN", "DDPG", "DDPGDiscretized"):
-        return globals()[config.agent_type].from_config(config)
-    raise NotImplementedError(f"Agent type {config.agent_type} not implemented")
-
+from .algorithm import make_agent
