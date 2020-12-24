7288e89130d9cfb82969040cb7a19ee89c9f7820
Hanwant <admin@madigan.tech>
2020-12-25 02:15:56 +1300

    isolated cpprb bug (filed issue on gitlab)

--- madigan/tests/custom_rb_vs_cpprb.py
+++ madigan/tests/custom_rb_vs_cpprb.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from madigan.utils import time_profile
 from madigan.utils.data import SARSD, State
-from madigan.utils import ReplayBuffer as RBP # RB Python
+from madigan.utils.replay_buffer import ReplayBuffer as RBP # RB Python
 from cpprb import ReplayBuffer as RBC # RB Cython
 
 buffer_size = 1_000_000
@@ -16,18 +16,20 @@ env_dict={"state_price": {"shape": obs_shape},
           "next_state_price": {"shape": obs_shape},
           "next_state_portfolio": {"shape": action_shape},
           "reward": {},
-          "done": {},
-          "discounts": {}}
+          "done": {}}
+          #"discounts": {}}
 
-def make_data_dict(n):
+def make_data_dict(n, nstep=None):
+    #if nstep is not None:
+     #   discounts = 
     data={"state_price": np.random.randn(n, obs_shape).astype(np.float32),
           "state_portfolio": np.random.randn(n, action_shape).astype(np.float32),
           "action": np.random.randn(n, action_shape).astype(np.float32),
           "reward": np.random.randn(n).astype(np.float32).astype(np.float32),
           "next_state_price": np.random.randn(n, obs_shape).astype(np.float32),
           "next_state_portfolio": np.random.randn(n, action_shape).astype(np.float32),
-          "done": np.random.binomial(1, 0.1, n),
-          "discounts": np.random.randn(n).astype(np.float32)}
+          "done": np.random.binomial(1, 0.1, n)}
+          #"discounts": np.random.randn(n).astype(np.float32)}
     return data
 
 def make_data_tuple(n):
@@ -59,6 +61,62 @@ def add_RBP_tuple(rbp, data):
 
 def add_RBC_dict(rbc, data):
     rbc.add(**data)
+
+def test_add_speed():
+    # rbp = RBP(buffer_size, 1, 0.9)
+    rbc = RBC(buffer_size, env_dict=env_dict)
+    data_dict = make_data_dict(32)
+    # data_tup = make_data_tuple(32)
+    # time_py_dict = timeit(lambda: add_RBP_dict(rbp, data_dict), number=1000)
+    # time_py_tuple = timeit(lambda: add_RBP_tuple(rbp, data_tup), number=1000)
+    time_cy = timeit(lambda: add_RBC_dict(rbc, data_dict), number=1000)
+    # print('timeit py add dict 32: ', time_py_dict)
+    # print('timeit py add tuple 32: ', time_py_tuple)
+    # print('timeit cy add dict 32: ', time_cy)
+    # print('doing time_profile')
+    # time_profile(1001, 0,
+    #              add_RBP_dict = lambda: add_RBP_dict(rbp, data_dict),
+    #              add_RBP_tuple = lambda: add_RBP_tuple(rbp, data_tup),
+    #              add_RBC_dict = lambda: add_RBC_dict(rbc, data_dict))
+
+def test_add_speed_nstep():
+    # rbp = RBP(buffer_size, 30, 0.99)
+    rbc = RBC(buffer_size, env_dict=env_dict, Nstep={"size": 30, "gamma": 0.99,
+                                                     "rew": "reward",
+                                                     "next": ["next_state_price",
+                                                              "next_state_port"]})
+    data_dict = make_data_dict(32)
+    # data_tup = make_data_tuple(32)
+    # time_py_dict = timeit(lambda: add_RBP_dict(rbp, data_dict), number=1000)
+    # time_py_tuple = timeit(lambda: add_RBP_tuple(rbp, data_tup), number=1000)
+    time_cy = timeit(lambda: add_RBC_dict(rbc, data_dict), number=1000)
+    # print('timeit py add dict 32: ', time_py_dict)
+    # print('timeit py add tuple 32: ', time_py_tuple)
+    # print('timeit cy add dict 32: ', time_cy)
+    # print('doing time profile')
+    # time_profile(1000, 0,
+    #              add_RBP_dict = lambda: add_RBP_dict(rbp, data_dict),
+                #  add_RBP_tuple = lambda: add_RBP_tuple(rbp, data_tup),
+                #  add_RBC_dict = lambda: add_RBC_dict(rbc, data_dict))
+
+def test_sampling_speed():
+    # rbp = RBP(buffer_size, 1, 0.99)
+    rbc = RBC(buffer_size, env_dict=env_dict)
+    data_dict = make_data_dict(32)
+    # import ipdb; ipdb.set_trace()
+    for i in range(1000):
+        # add_RBP_dict(rbp, data_dict)
+        add_RBC_dict(rbc, data_dict)
+
+    rbc.on_episode_end()
+    # time_py_sample = timeit(lambda: rbp.sample(32), number=1000)
+    time_cy_sample = timeit(lambda: rbc.sample(32), number=1000)
+    # print('py sample time: ', time_py_sample)
+    print('cy sample time: ', time_cy_sample)
+    # print('doing time profile')
+    # time_profile(1000, 0, py_sample=lambda: rbp.sample(32),
+    #              cy_sample=lambda: rbc.sample(32))
+
 
 def test_sarsd_same(sample_py, sample_cy):
     for i, (a, b) in enumerate(zip([sample_py.state.price, sample_py.state.portfolio,
@@ -100,62 +158,6 @@ def test_nstep_logic():
     sample_cy = rbc.get_all_transitions()
     # import ipdb; ipdb.set_trace()
     test_sarsd_same(sample_py, sample_cy)
-
-
-def test_add_speed():
-    rbp = RBP(buffer_size, 1, 0.9)
-    rbc = RBC(buffer_size, env_dict=env_dict)
-    data_dict = make_data_dict(32)
-    data_tup = make_data_tuple(32)
-    time_py_dict = timeit(lambda: add_RBP_dict(rbp, data_dict), number=1000)
-    time_py_tuple = timeit(lambda: add_RBP_tuple(rbp, data_tup), number=1000)
-    time_cy = timeit(lambda: add_RBC_dict(rbc, data_dict), number=1000)
-    print('timeit py add dict 32: ', time_py_dict)
-    print('timeit py add tuple 32: ', time_py_tuple)
-    print('timeit cy add dict 32: ', time_cy)
-    print('doing time_profile')
-    time_profile(1000, 0,
-                 add_RBP_dict = lambda: add_RBP_dict(rbp, data_dict),
-                 add_RBP_tuple = lambda: add_RBP_tuple(rbp, data_tup),
-                 add_RBC_dict = lambda: add_RBC_dict(rbc, data_dict))
-
-def test_add_speed_nstep():
-    rbp = RBP(buffer_size, 30, 0.99)
-    rbc = RBC(buffer_size, env_dict=env_dict, Nstep={"size": 30, "gamma": 0.99,
-                                                     "rew": "reward",
-                                                     "next": ["next_state_price",
-                                                              "next_state_port"]})
-    data_dict = make_data_dict(32)
-    data_tup = make_data_tuple(32)
-    time_py_dict = timeit(lambda: add_RBP_dict(rbp, data_dict), number=1000)
-    time_py_tuple = timeit(lambda: add_RBP_tuple(rbp, data_tup), number=1000)
-    time_cy = timeit(lambda: add_RBC_dict(rbc, data_dict), number=1000)
-    print('timeit py add dict 32: ', time_py_dict)
-    print('timeit py add tuple 32: ', time_py_tuple)
-    print('timeit cy add dict 32: ', time_cy)
-    print('doing time profile')
-    time_profile(1000, 0,
-                 add_RBP_dict = lambda: add_RBP_dict(rbp, data_dict),
-                 add_RBP_tuple = lambda: add_RBP_tuple(rbp, data_tup),
-                 add_RBC_dict = lambda: add_RBC_dict(rbc, data_dict))
-
-def test_sampling_speed():
-    rbp = RBP(buffer_size, 1, 0.99)
-    rbc = RBC(buffer_size, env_dict=env_dict)
-    data_dict = make_data_dict(32)
-    for i in range(1000):
-        add_RBP_dict(rbp, data_dict)
-        add_RBC_dict(rbc, data_dict)
-
-    rbc.on_episode_end()
-    time_py_sample = timeit(lambda: rbp.sample(32), number=1000)
-    time_cy_sample = timeit(lambda: rbc.sample(32), number=1000)
-    print('py sample time: ', time_py_sample)
-    print('cy sample time: ', time_cy_sample)
-    print('doing time profile')
-    time_profile(1000, 0, py_sample=lambda: rbp.sample(32),
-                 cy_sample=lambda: rbc.sample(32))
-
 
 if __name__ == "__main__":
     print("*"*80)
