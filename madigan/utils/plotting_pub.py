f4892477c1233d70f9f9ef15807e93bbe65fd8d0
Hanwant <admin@madigan.tech>
2021-01-05 00:37:56 +1300

    make better plots for pub

--- madigan/utils/plotting_pub.py
+++ madigan/utils/plotting_pub.py
@@ -3,22 +3,30 @@ Utils for making figures for publication or presentation
 """
 import os
 from pathlib import Path
+from typing import Union
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set()
+sns.set_context("paper", font_scale=1.5)
+
 
 def save_fig(fig, savepath):
     """ Save all figures in pdf format - best for latex """
-    fig.save(savepath, format='pdf')
+    savepath = Path(savepath)
+    if not savepath.parent.is_dir():
+        savepath.parent.mkdir(parents=True, exist_ok=True)
+    fig.savefig(savepath, format='pdf', bbox_inches='tight')
 
-def get_episode_id(experiment_id,
-                   basepath,
-                   env_steps=None,
-                   episode_steps=None,
-                   ):
+
+def get_episode_id(
+    experiment_id,
+    basepath,
+    env_steps=None,
+    episode_steps=None,
+):
     assert env_steps is not None or episode_steps is not None
     path = Path(basepath) / experiment_id
     runs = filter(lambda x: "episode" in str(x.name), path.iterdir())
@@ -51,19 +59,60 @@ def load_test_run_data(episode_id, experiment_id, basepath):
     return df
 
 
-def make_train_figs(experiment_id, basepath, show=True, save=True,
-                    savepath=None):
-    df = load_train_data(experiment_id, basepath)
-    savepath = savepath or Path(os.getcwd())/experiment_id
-    figs = []
+def make_train_figs(experiment_id, basepath):
+    df = load_train_data(experiment_id, basepath).reset_index(drop=True)
+    figs = {}
     for label in df.columns:
-        fig, ax = plt.subplot(1, 1, figsize=(15, 10))
+        fig, ax = plt.subplots(1, 1)
         ax.plot(df[label], label=label)
         ax.legend()
-        figs.append({'fig': fig, 'fname': label})
+        ax.set_xlabel('training_step')
+        # figs.append(fig)
+        # figs.append({'fig': fig, 'fname': label})
+        figs[label] = fig
+    figs['running_reward'].axes[0].set_ylabel('cumulative rewards')
+    return figs
+
+
+def make_multi_line_plot(dat, ax, assets=None):
+    n_assets = dat.shape[1]
+    assets = assets if assets is not None else [
+        f"asset_{i}" for i in range(n_assets)
+    ]
+    if n_assets != len(assets):
+        raise ValueError("data shape does not match number of assets provided")
+    for i, asset in enumerate(assets):
+        ax.plot(dat[:, i], label=asset)
+    ax.legend(loc='upper left')
+    return ax
+
+
+
+def make_figs(data: Union[dict, pd.DataFrame], assets=None):
+    figs = {}
+    for label in data.keys():
+        try:
+            # dat = data[label].squeeze()
+            dat = data[label]
+            fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+            if len(dat.shape) == 1:
+                ax.plot(dat, label=label)
+                ax.legend(loc='upper left')
+            elif len(dat.shape) == 2:
+                ax = make_multi_line_plot(dat, ax, assets=assets)
+            else:
+                ax.imshow(dat.squeeze())  # vmin=0., vmax=1.) #, cmap='gray'
+            ax.set_ylabel(label)
+            ax.set_xlabel('step')
+            figs[label] = fig
+        except Exception as E:
+            print(E)
+            print('skipping: ', label)
     return figs
 
 
 def save_figs(figs, savepath):
-    for fig in figs:
-        save_fig(fig['fig'], savepath/f"{fig['fname']}.pdf")
+    for label, fig in figs.items():
+        save_fig(fig, savepath / f"{label}.pdf")
+        # save_fig(fig['fig'], savepath/f"{fig['fname']}.pdf")
+        # save_fig(fig, savepath/f'{fig.label}.pdf')
